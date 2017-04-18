538f053b069a72102463e9b6b9066068fb175df1
李太新 <toonew@danwi.me>
2017-04-18 17:47:59 +0800

    zip 3部分，以及 该库中分析的理解

--- test/test.js
+++ test/test.js
@@ -6,5 +6,5 @@ const fs = require('fs');
 const unzip = require('../index');
 
 var rs = fs.createReadStream('./test.zip');
-var ws = unzip.Extract({path: 'output/'});
+var ws = unzip.Extract({path: 'output', verbose: true});//1.输出路径 2.想信息开关
 rs.pipe(ws);
